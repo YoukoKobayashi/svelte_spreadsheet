4408719d3bfc2338d2af4e63dcd22407c95c31ac
YoukoKobayashi <kobayouyou@gmail.com>
2023-01-24 14:42:40 +0900

    2nd commit!

---
+++ src/routes/spreadsheet/Cell.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let content:String|number = '';
+</script>
+
+<td>{content}</td>
---
+++ src/routes/spreadsheet/Spreadsheet.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+import Cell from './Cell.svelte';
+let editing=false;
+</script>
+
+
+<table>
+  <thead>
+  <tr>
+    <th></th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>1</th>
+<Cell content={1} />
+<Cell content={2} />
+<Cell content={3} />
+    </tr>
+    <tr>
+      <th>2</th>
+<Cell content={4} />
+<Cell content={5} />
+<Cell content={6} />
+    </tr>
+    <tr>
+      <th>3</th>
+<Cell content={7} />
+<Cell content={8} />
+<Cell content={9} />
+    </tr>
+  </tbody>
+</table>
+
+
+
+<style>
+table{
+  width:100%;
+  border-collapse:collapse;
+}
+th,td{
+  color:#505050;
+  font-weight:normal;
+  border:1px solid #999;
+  background:#fff;
+  padding:0;
+  margin:0;
+}
+</style>
